976388a25f60684ec966fb77a17bab6b30f68a9c
PavelSher <4829872@rambler.ru>
2020-03-17 17:44:13 +0200

    part 3 of task

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
   rescue_from ActiveRecord::RecordNotFound, with: :rescue_with_not_found
 
   def default_url_options
-    { lang: I18n.locale }
+    { lang: (I18n.locale unless I18n.locale == I18n.default_locale) }
   end
 
 
