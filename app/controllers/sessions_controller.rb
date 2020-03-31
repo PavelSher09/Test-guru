73e5d464cbaa5a4f9ed50862030a8666ace6f4f3
PavelSher <4829872@rambler.ru>
2020-03-31 12:40:41 +0300

    initial bootstrap

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,5 @@
 class SessionsController < Devise::SessionsController
 
-   
   def create
     super
     if current_user && current_user.sign_in_count == 1
@@ -8,9 +7,6 @@ class SessionsController < Devise::SessionsController
         flash.notice = "Welcome, #{current_user.first_name}"
         session[:display_welcome] = true
       end
-      
     end
-    
   end
-  
 end
